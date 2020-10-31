bd7af471873fd1be24a2157dcb10ab5d5c12c56f
kri99 <1>
2020-10-31 21:21:44 +0100

    added minminArray.js

---
+++ minArray.js
@@ -0,0 +1,11 @@
+// function seeks smallest number in array
+function findMin( n ) {
+	let min = Infinity;
+	n.forEach( ( el ) => {
+		min = ( el < min ) ? el : min; 
+	});
+	return min;
+	
+} 
+//example
+console.log(findMin([1,3,-5,6,8,9])); //-5
